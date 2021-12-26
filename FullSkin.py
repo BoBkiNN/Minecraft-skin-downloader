8cc6b8c2350dcec98cea2685408f88716fec222a
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2021-12-26 22:26:57 +0600

    Update FullSkin.py

--- FullSkin.py
+++ FullSkin.py
@@ -8,19 +8,21 @@ def app():
 	urlt = f"https://tlauncher.org/upload/all/nickname/tlauncher_{nick}.png"
 	name = url.split("/")
 	name = name[len(name)-1]
-	print(f"Skin will be saved as {name}")
 
 	if os.path.exists(name):
 	    	    os.remove(name)
 	try:
 			wget.download(url,name)
+			fname = name
 	except:
 		try:
 			wget.download(urlt, f"tlauncher_{name}")
+			fname = f"tlauncher_{name}"
 		except:
 			print(f"Nickname {nick} not found")
 			print("")
 			time.sleep(1)
 			app()
+	print(f"Skin was saved as {fname}")
 	app()
 app()
