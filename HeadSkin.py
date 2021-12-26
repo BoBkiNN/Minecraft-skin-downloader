30eabb792da0517bee4ef38a9ce6ee6995ca10f1
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2021-12-26 22:28:24 +0600

    Update HeadSkin.py

--- HeadSkin.py
+++ HeadSkin.py
@@ -6,24 +6,30 @@ import time
 def app():
 	nick = input("Nickname: ")
 	url = f"https://tlauncher.org/upload/all/	nickname/{nick}.png"
+	urlt = f"https://tlauncher.org/upload/all/nickname/tlauncher_{nick}.png"
 	name = url.split("/")
 	name = name[len(name)-1]
 	#print(f"Skin will be saved as {name} in {os.getcwd()}")
 	if os.path.exists(name):
     	    os.remove(name)
 	try:
-		wget.download(url,name)
+			wget.download(url,name)
+			fname = name
 	except:
-		print(f"Nickname {nick} not found")
-		print("")
-		time.sleep(1)
-		app()
+		try:
+			wget.download(urlt, f"tlauncher_{name}")
+			fname = f"tlauncher_{name}"
+		except:
+			print(f"Nickname {nick} not found")
+			print("")
+			time.sleep(1)
+			app()
  
-	im = Image.open(name)
+	im = Image.open(fname)
 	im_crop = im.crop((8,8, 16,16))
-	print(f"Head will be saved as 'head-{name}' in {os.getcwd()}")
+	print(f"Head was saved as 'head-{fname}' in {os.getcwd()}")
 
-	im_crop.save(f'head-{name}')
-	os.remove(name)
+	im_crop.save(f'head-{fname}')
+	os.remove(fname)
 	app()
 app()
